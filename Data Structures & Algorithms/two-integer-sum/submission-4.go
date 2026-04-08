ee6fd0dae9da499266ee0e50fd2e1744761f8d12
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2026-04-08 10:08:09 -0300

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.go
@@ -0,0 +1,13 @@
+func twoSum(nums []int, target int) []int {
+    m := make(map[int]int)
+    
+    for i := 0; i < len(nums); i++ {
+        diff := target - nums[i]
+        if j, ok := m[diff]; ok {
+            return []int{j, i}
+        }
+        m[nums[i]] = i
+    }
+
+    return []int{-1, -1}
+}
